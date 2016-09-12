e0d60064e8f6c9617a6989383ec23b3963340a6b
Furkan Kadıoğlu <furkan@castram.com>
2016-09-12 19:56:25 +0300

    Pages > Static Dummy Data

--- app/Modules/Pages/Database/migrations/2016_01_01_010101_StaticDatas.php
+++ app/Modules/Pages/Database/migrations/2016_01_01_010101_StaticDatas.php
@@ -18,6 +18,42 @@ class StaticDatas extends Migration
             $table->string('type');
             $table->text('content');
         });
+
+        \DB::table('pages_static_datas')->insert(array (
+            0 => 
+            array (
+                'id' => 8,
+                'slug' => 'anabaslik',
+                'type' => 'Contents',
+                'content' => 'Modvele Hoşgeldiniz!',
+            ),
+            1 => 
+            array (
+                'id' => 9,
+                'slug' => 'anaicerik',
+                'type' => 'Contents',
+                'content' => 'Bu otomatik olarak oluşturulmuş bir içeriktir.',
+            ),
+            2 => 
+            array (
+                'id' => 14,
+                'slug' => 'homepage',
+                'type' => 'Homepage',
+            'content' => '@extends(\'masters.main\') 
+
+            @section(\'content\')
+            <div class="jumbotron">
+            <h1>[-anabaslik-]</h1>
+            <p>[-anaicerik-]</p>
+            @if(!Auth::user()) 
+            <p>
+            <a class="btn btn-lg btn-primary" href="{{ url(\'/Users/login\') }}" role="button">Giriş Yap »</a>
+            </p>
+            @endif
+            </div>
+            @endsection',
+            ),
+        ));
     }
 
     /**
